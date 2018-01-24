87889f4ebb20aa04734c55cbef8a15b8900a554f
thbchr <33120849+thbchr@users.noreply.github.com>
2018-01-24 03:45:15 +0100

    Add two pages for Qualitative attributes
    - qualitative.tpl : equivalent of attributes.tpl for qualitative attributes (manage the attributes, creation/edition)
    - words.tpl : equivalent of questions.tpl for qualitative attributes (assessment to define an utility function)

--- website.py
+++ website.py
@@ -104,6 +104,19 @@ def credits():
         return template('authentification', get_url=app.get_url)
     return {'get_url':  app.get_url}
 
+@app.route('/qualitative')
+@view('qualitative')
+def qualitative():
+    if check_passwd(request.get_cookie("mdp")) == False:
+        return template('authentification', get_url=app.get_url)
+    return {'get_url':  app.get_url}
+
+@app.route('/words')
+@view('words')
+def words():
+    if check_passwd(request.get_cookie("mdp")) == False:
+        return template('authentification', get_url=app.get_url)
+    return {'get_url':  app.get_url}
 
 @app.route('/ajax', method="POST")
 def ajax():
